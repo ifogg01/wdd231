3a26b03170cfab936a9f39972b3c2f6dbe3772b7
ifogg01 <ifogg@byui.edu>
2024-11-05 16:10:00 -0700

    Week1Submit

---
+++ scripts.js
@@ -0,0 +1,20 @@
+
+function formatDate(date) {
+    const options = { year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit', second: '2-digit' };
+    return date.toLocaleDateString('en-US', options);
+}
+
+
+function updateLastModified() {
+    const lastModifiedDate = new Date(document.lastModified);
+    document.getElementById("lastModified").textContent = formatDate(lastModifiedDate);
+}
+
+
+document.addEventListener("DOMContentLoaded", updateLastModified);
+
+
+function toggleMenu() {
+    const navbar = document.querySelector('.navbar');
+    navbar.classList.toggle('show');
+}
