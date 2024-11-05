f0f5ac80b712750b29a2531d6154e0cd7001d200
ifogg01 <ifogg@byui.edu>
2024-11-05 16:34:50 -0700

    FixHamburgerMenu

--- scripts.js
+++ scripts.js
@@ -1,20 +1,18 @@
-
 function formatDate(date) {
     const options = { year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit', second: '2-digit' };
     return date.toLocaleDateString('en-US', options);
 }
-
 
 function updateLastModified() {
     const lastModifiedDate = new Date(document.lastModified);
     document.getElementById("lastModified").textContent = formatDate(lastModifiedDate);
 }
 
-
 document.addEventListener("DOMContentLoaded", updateLastModified);
-
 
 function toggleMenu() {
     const navbar = document.querySelector('.navbar');
+    const hamburger = document.querySelector('.hamburger');
     navbar.classList.toggle('show');
+    hamburger.classList.toggle('open');
 }
